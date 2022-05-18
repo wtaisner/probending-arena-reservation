a0d432d56de636caae3c70bc2b8932bb59151a2f
wtaisner <taisner.witold@gmail.com>
2022-05-18 21:16:59 +0200

    initialize database framework

--- src/CassandraConnector.py
+++ src/CassandraConnector.py
@@ -1,4 +1,5 @@
 from cassandra.cluster import Cluster
+from db.entity import Entity
 
 
 class CassandraConnector:
@@ -7,10 +8,24 @@ class CassandraConnector:
         self.cluster = Cluster([ip_address], port)
         self.session = None
         self._create_session(keyspace)
+        self._create_tables()
 
-    def _create_session(self, keyspace):
+    def _create_session(self, keyspace: str, replication_factor: int = 3, strategy: str = 'SimpleStrategy') -> None:
+        """
+        Initialize cassandra session and create keyspace
+        :param keyspace: a string with the desired keyspace name
+        """
         self.session = self.cluster.connect()
-        command = f"CREATE KEYSPACE IF NOT EXISTS {keyspace} " + " WITH REPLICATION = {'class': 'SimpleStrategy','replication_factor': 3};"
+        command = f"CREATE KEYSPACE IF NOT EXISTS {keyspace}  WITH REPLICATION = " + \
+                  "{" + f"'class': {strategy},'replication_factor': {replication_factor}" \
+                  + "};"
+
         self.session.execute(command)
         self.session.set_keyspace(keyspace)
 
+    def _create_tables(self) -> None:
+        """
+        Initialize tables according to the schemas available in db.Entity
+        """
+        for table in Entity.tables:
+            self.session.execute(table)
---
+++ src/db/entity.py
@@ -0,0 +1,11 @@
+class Entity:
+
+    arena = "CREATE TABLE IF NOT EXISTS arena ... "
+
+    seat = "CREATE TABLE IF NOT EXISTS seat ..."
+
+    reservation = "CREATE TABLE IF NOT EXISTS reservation ..."
+
+    game = "CREATE TABLE IF NOT EXISTS game ..."
+
+    tables = [arena, seat, reservation, game]
