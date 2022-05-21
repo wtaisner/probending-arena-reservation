425f9db422179992c47fe0974ffaf8274af25d42
wtaisner <taisner.witold@gmail.com>
2022-05-21 12:52:59 +0200

    limit docker memory size for development

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,8 +1,8 @@
-./cleanup.sh
+#./cleanup.sh
 
-docker run --name cass1 -p 9042:9042 -e CASSANDRA_CLUSTER_NAME=ProbendingCluster -d cassandra:latest
-docker run --name cass2 -e CASSANDRA_SEEDS="$(docker inspect --format='{{.NetworkSettings.IPAddress}}' cass1)" -e CASSANDRA_CLUSTER_NAME=ProbendingCluster -d cassandra:latest
-docker run --name cass3 -e CASSANDRA_SEEDS="$(docker inspect --format='{{.NetworkSettings.IPAddress}}' cass1)" -e CASSANDRA_CLUSTER_NAME=ProbendingCluster -d cassandra:latest
+docker run --name cass1 -p 9042:9042 -e CASSANDRA_CLUSTER_NAME=ProbendingCluster -e HEAP_NEWSIZE=1M -e MAX_HEAP_SIZE=1024M -d cassandra:latest
+docker run --name cass2 -e CASSANDRA_SEEDS="$(docker inspect --format='{{.NetworkSettings.IPAddress}}' cass1)" -e CASSANDRA_CLUSTER_NAME=ProbendingCluster -e HEAP_NEWSIZE=1M -e MAX_HEAP_SIZE=1024M -d cassandra:latest
+docker run --name cass3 -e CASSANDRA_SEEDS="$(docker inspect --format='{{.NetworkSettings.IPAddress}}' cass1)" -e CASSANDRA_CLUSTER_NAME=ProbendingCluster -e HEAP_NEWSIZE=1M -e MAX_HEAP_SIZE=1024M -d cassandra:latest
 
 docker inspect --format='{{.NetworkSettings.IPAddress}}' cass1
 docker inspect --format='{{.NetworkSettings.IPAddress}}' cass2
