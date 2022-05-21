ec3ce2880e0015eb04e4ec9e377a6bdcbdb2872d
wtaisner <taisner.witold@gmail.com>
2022-05-21 12:53:12 +0200

    add simple framework for queries

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+import pyinputplus as pyip
+
 from src.CassandraConnector import CassandraConnector
 from src.QueryEngine import QueryEngine
 
 
+def connect() -> CassandraConnector:
+    """
+    utility function that return CassandraConnector object, which contains a session connected to Cassandra cluster
+    :return: connected cassandra session
+    """
+    return CassandraConnector("172.17.0.2", 9042, 'atla')
+
+
 def main() -> None:
-
-    client = CassandraConnector("172.17.0.2", 9042, 'atla')
-
-    query = QueryEngine()
-
-    res = client.session.execute("SELECT * FROM arena")
-    print(res.one())
-    res = client.session.execute("SELECT * FROM seat")
-    print(res.one())
+    client = connect()
+    while True:
+        result = pyip.inputStr('Please insert your query: ')
+        if result == 'grzenda-menda':
+            break
+        else:
+            try:
+                res = client.session.execute(result)
+                print(res.all())
+            except ValueError:
+                raise ValueError('Invalid query you idiot')
 
 
 if __name__ == '__main__':
