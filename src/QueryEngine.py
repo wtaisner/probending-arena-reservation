14feedb5021a2ac0ef32252fe88057621e22a7fd
annprzy <anneprzybylowska@gmail.com>
2022-05-21 19:14:29 +0200

    implement query engine functions, add requirements

--- src/QueryEngine.py
+++ src/QueryEngine.py
@@ -1,3 +1,7 @@
+from typing import Any, List
+from uuid import UUID
+
+
 class QueryEngine:
     """
     Would you like some tea, stranger?
@@ -6,14 +10,38 @@ class QueryEngine:
     def __init__(self):
         pass
 
-    def update_record(self, table_name: str, *args) -> str:
-        pass
+    def _map_to_str(self, column_type, value):
+        if column_type == 'text' or column_type == "UUID":
+            return f"'{value}'"
+        else:
+            return f"{value}"
 
-    def query_record(self, table_name: str, *args) -> str:
-        pass
+    def update_record(self, table_name: str, column: str, column_type: str, value: Any, where_column: str, where_value: UUID, *args) -> str:
+        query = f"UPDATE {table_name} SET {column} = {self._map_to_str(column_type, value)} "\
+            f"WHERE {where_column} = '{where_value}' IF EXISTS;"
+        return query
 
-    def delete_record(self, table_name: str, *args) -> str:
-        pass
+    def query_record(self, table_name: str, columns: str, where_column: str, column_type: str, where_value: Any, *args) -> str:
+        query = f"SELECT {columns} from {table_name} "\
+            f"WHERE {where_column} = {self._map_to_str(column_type, where_value)};"
+        return query
 
-    def insert_record(self, table_name: str, *args) -> str:
-        pass
+    def delete_record(self, table_name: str, where_column: str, where_value: UUID, *args) -> str:
+        query = f"DELETE FROM {table_name} WHERE {where_column} = '{where_value}' IF EXISTS;"
+        return query
+
+    def insert_record(self, table_name: str, columns: List[str], columns_types: List[str], values: List[Any], *args) -> str:
+
+        columns_str = ""
+        values_str = ""
+        for column, column_type, value in zip(columns, columns_types, values):
+            columns_str += column
+            columns_str += ", "
+            values_str += self._map_to_str(column_type, value)
+            values_str += ", "
+
+        values_str = values_str[:-2]
+        columns_str = columns_str[:-2]
+
+        query = f"INSERT INTO {table_name} ({columns_str}) VALUES ({values_str});"
+        return query
--- src/db/initial_entities.py
+++ src/db/initial_entities.py
@@ -3,6 +3,8 @@ from typing import List
 
 import uuid
 
+from src.QueryEngine import QueryEngine
+
 
 class God:
     """
@@ -10,10 +12,15 @@ class God:
     """
 
     def __init__(self):
+        self.arena_columns = ['arena_id', 'name', 'address']
+        self.arena_columns_types = ['UUID', 'text', 'text']
+        self.seats_columns = ['seat_id', 'arena_id']
+        self.seats_columns_types = ['UUID', 'UUID']
         self.arenas = ["Uncles Iroh Arena", "Sokkas Arena"]
         self.arenas_id = [uuid.uuid4() for _ in range(len(self.arenas))]
         self.address = ['Ba Sing Se', 'Capitol City']
         self.teams = ["Dragons of the West", "Fire Ferrets"]
+        self.query_engine = QueryEngine()
 
     def populate(self) -> List[str]:
         queries = [self._init_arenas(), self._init_seats()]
@@ -25,10 +32,8 @@ class God:
     def _init_arenas(self) -> str:
         query = 'BEGIN BATCH '
         for arena, arena_id, address in zip(self.arenas, self.arenas_id, self.address):
-            # arena_id = uuid.uuid4()
-            query += "INSERT INTO arena (arena_id, name, address) VALUES " \
-                     + f"('{arena_id}', '{arena}', '{address}');"
-            # self.arenas_id.append(arena_id)
+            query += self.query_engine.insert_record(
+                'arena', self.arena_columns, self.arena_columns_types, [arena_id, arena, address])
         return query + " APPLY BATCH; "
 
     def _init_seats(self) -> str:
@@ -36,7 +41,7 @@ class God:
         for arena_id in self.arenas_id:
             seat_id = [uuid.uuid4() for _ in range(10)]
             for idx in seat_id:
-                query += "INSERT INTO seat (seat_id, arena_id) VALUES " \
-                         + f"('{idx}', '{arena_id}');"
+                query += self.query_engine.insert_record(
+                    'seat', self.seats_columns, self.seats_columns_types, [idx, arena_id])
 
         return query + " APPLY BATCH; "
